d94687d18f527509f4ecd43225ba0ff5f96f13b7
Ihor Ivanets <ivanets22@gmail.com>
2024-08-06 09:23:28 +0300

    completed hw 9

---
+++ src/js/2-form.js
@@ -0,0 +1,42 @@
+const feedbackFormEl = document.querySelector('.js-feedback-form');
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const fillFormFields = () => {
+  const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form'));
+
+  if (formDataFromLS === null) {
+    return;
+  }
+
+  formData = formDataFromLS;
+
+  const formDataFromLSKeys = Object.keys(formDataFromLS);
+  for (const key of formDataFromLSKeys) {
+    feedbackFormEl.elements[key].value = formDataFromLS[key];
+  }
+};
+
+fillFormFields();
+
+const onFormFieldInput = event => {
+  const fieldName = event.target.name;
+  const fieldValue = event.target.value.trim();
+
+  formData[fieldName] = fieldValue;
+
+  localStorage.setItem('feedback-form', JSON.stringify(formData));
+};
+
+const onFeedbackFormSubmit = event => {
+  event.preventDefault();
+
+  event.target.reset();
+  localStorage.removeItem('feedback-form');
+};
+
+feedbackFormEl.addEventListener('input', onFormFieldInput);
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
